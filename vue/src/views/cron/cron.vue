9215d700e340997465e11cefc8e9c3a7cb01e373
lieoxc <xiaocongli45@gmail.com>
2023-04-12 02:05:19 +0800

    feat【定时任务页面】页面布局和交互逻辑完成80%

--- vue/src/views/cron/cron.vue
+++ vue/src/views/cron/cron.vue
@@ -1,35 +1,59 @@
 <template>
     <div>
         <Card dis-hover bordered>
+            <!-- 页面头部标题 -->
             <p slot="title">
                 <Icon type="ios-alarm-outline" /> 定时任务管理
             </p>
+
+            <!-- 表格头部 增删改的按钮  -->
             <Form inline>
                 <FormItem>
                     <Button type="primary" icon="ios-add-circle-outline" style="margin-right: 10px"
-                        @click="showInput" >新增</Button>
-                    <Button type="info" icon="ios-create" style="margin-right: 10px"
-                    @click="showModify" :disabled="modifyButtomStatus" >修改</Button>
-                    <Button type="error" icon="md-trash" :disabled="deleteButtomStatus" > 删除</Button>
+                        @click="showInput">新增</Button>
+                    <!-- <Button type="info" icon="ios-create" style="margin-right: 10px" @click="showModify"
+                        :disabled="disableModifyButtom">修改</Button> -->
+                    <Button type="error" icon="md-trash" :disabled="disableDeleteButtom"> 删除</Button>
                 </FormItem>
             </Form>
+
+             <!-- 按编辑或者添加的 弹窗 -->
             <Modal v-model="showModal" title="添加定时任务" :closable="false" @on-ok="handleOk">
-                <Form :model="form">
+                <Form :model="inputTableFrom">
                     <FormItem label="名称">
                         <Input v-model="inputTableFrom.name" />
                     </FormItem>
                     <FormItem label="cron表达式">
-                        <Input v-model="inputTableFrom.cron" />
+                        <Input v-model="inputTableFrom.content" />
                     </FormItem>
                     <FormItem label="调用目标">
-                        <Input v-model="inputTableFrom.func" />
+                        <Input v-model="inputTableFrom.callFunc" />
                     </FormItem>
                     <FormItem label="任务状态">
-                        <Input v-model="inputTableFrom.status" />
+                        <Select v-model="inputTableFrom.status">
+                            <Option v-for="item in workStatus" :value="item.value" :key="item.value">{{ item.label }}
+                            </Option>
+                        </Select>
                     </FormItem>
+
                 </Form>
             </Modal>
-            <Table stripe border ref="selection" height="200" :columns="columns1" :data="tableData">
+
+            <!-- 表格头部 -->
+            <Table stripe border ref="selection" height="200px" @on-selection-change="tableSelectChange" :columns="columns1"
+                :data="tableData">
+                <template slot-scope="{ row }" slot="status">
+                    <div v-if = "row.status == 'Start'">
+                        <strong>启用</strong>
+                    </div>   
+                    <div v-else>
+                        <strong>禁用 </strong>
+                    </div>  
+                </template>
+                <template slot-scope="{ row, index }" slot="action">
+                    <Button type="info" size="small" style="margin-right: 5px" @click="modify(row)">编辑</Button>
+                    <Button type="error" size="small" @click="remove(row)">删除</Button>
+                </template>
             </Table>
         </Card>
     </div>
@@ -65,30 +89,13 @@ export default {
                 },
                 {
                     title: '状态',
-                    key: 'status',
+                    slot: 'status',
                 },
                 {//操作按钮
-                    title: 'Action',
-                    key: 'action',
+                    title: '操作',
+                    slot: 'action',
                     fixed: 'right',
                     width: 150,
-                    render: (h, params) => {
-                        return h('div', [
-                            h('Button', {
-                                props: {
-                                    type: 'info',
-                                    size: 'small',
-                                },
-                                style: "margin-right: 10px"
-                            }, '修改'),
-                            h('Button', {
-                                props: {
-                                    type: 'error',
-                                    size: 'small'
-                                }
-                            }, '删除')
-                        ]);
-                    }
                 }
             ],
             tableData: [
@@ -97,45 +104,79 @@ export default {
                     name: 'John Brown',
                     content: "0/5 * * * *",
                     callFunc: "heloworld",
-                    status: '停用',
+                    status: 'Stop',
                 },
                 {
                     id: 2,
                     name: 'John Brown',
                     content: "0/56 * * * *",
                     callFunc: "heloworld",
-                    status: '停用',
+                    status: 'Stop',
                 },
                 {
                     id: 2,
                     name: 'John Brown',
                     content: "0/2 * * * *",
                     callFunc: "heloworld",
-                    status: '启用',
+                    status: 'Start',
                 }
             ],
             inputTableFrom: {
                 name: "",
-                cron: "",
-                func: "",
+                content: "",
+                callFunc: "",
                 status: "",
             },
+            workStatus: [
+                {
+                    value: 'Start',
+                    label: '启用'
+                },
+                {
+                    value: 'Stop',
+                    label: '禁用'
+                }
+            ],
             showModal: false, // 是否显示弹窗
-            modifyButtomStatus:true , //是否使能修改按钮
-            deleteButtomStatus:true //是否使能删除按钮
+            //disableModifyButtom: true, //是否关闭修改按钮
+            disableDeleteButtom: true, //是否闭删除按钮
+            multipleSelection: []  //多选状态
         }
     },
     methods: {
         showInput() {
             this.showModal = true;
         },
-        showModify(){
+        showModify() {
             this.showModal = true;
         },
         handleOk() {
             console.log('输入框的值是：', this.inputValue);
             this.showModal = false;
         },
+        tableSelectChange(row) {
+            this.multipleSelection = []  //初始化清空
+            row.forEach(item => {
+                this.multipleSelection.push(item.id)
+            }) //将选中的值  id保存到 multipleSelection数组
+            console.log('table select ', this.multipleSelection);
+            if (this.multipleSelection.length  >0 ) { //单选的时候支持 删除和修改
+                this.disableDeleteButtom = false
+                //this.disableModifyButtom = false
+            } else { //未选中是，删除和修改都不支持
+                this.disableDeleteButtom = true
+                //this.disableModifyButtom = true
+            }
+
+        },
+        remove(row) {
+            console.log('remove index ', row);
+        },
+        modify(row) {
+            this.showModal = true; //打开弹框
+            this.inputTableFrom = { ...row }; //填充弹窗数据
+            console.log('remove index ', row);
+        }
     }
 };
 </script>
